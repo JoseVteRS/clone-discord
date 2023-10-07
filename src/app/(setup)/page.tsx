26ae83e8705351e317ff178b17dfb3f29dc02f60
Jose <jvrs.90@gmail.com>
2023-10-07 20:53:44 +0200

    change: setup page

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -5,27 +5,27 @@ import { initialProfile } from "@/lib/initial-profile";
 import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
-  // const profile = await initialProfile();
+  const profile = await initialProfile();
 
-  // const server = await db.server.findFirst({
-  //   where: {
-  //     members: {
-  //       some: {
-  //         profileId: profile.id
-  //       }
-  //     }
-  //   }
-  // });
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
 
-  // if (server) {
-  //   return redirect(`/servers/${server.id}`);
-  // }
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
 
-  // return <InitialModal />;
+  return <InitialModal />;
 
-  return (
-    <h1>Initial point</h1>
-  )
+  // return (
+  //   <h1>Initial point</h1>
+  // )
 }
- 
+
 export default SetupPage;
