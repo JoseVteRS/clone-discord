3fb9cbb70ee81e8c356a102a06c7d8b67c7b66e2
Jose <jvrs.90@gmail.com>
2023-10-08 22:04:18 +0200

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -6,9 +7,21 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     // Make sure you don't have "/" inside of publicRoutes
     publicRoutes: ["/api/uploadthing"],
+    afterAuth(auth, req, evt) {
+        // handle users who aren't authenticated
+        if (!auth.userId && !auth.isPublicRoute) {
+          return redirectToSignIn({ returnBackUrl: req.url });
+        }
+        // redirect them to organization selection page
+        if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/"){
+          const orgSelection = new URL('/', req.url)
+          return NextResponse.redirect(orgSelection)
+        }
+      }
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
 
